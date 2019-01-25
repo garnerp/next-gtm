36308733c3dfd24d8d03952faedb4a09927d7309
Garner <garner.pd@pg.com>
2019-01-25 15:17:32 -0500

    added GTM

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,18 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <style>{`body { margin: 0 } /* custom! */`}</style>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TCX44CK');
+          `
+            }}
+          />
         </Head>
         <body className="custom_class">
           <Main />
