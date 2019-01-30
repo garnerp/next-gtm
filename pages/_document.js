cce63ad93dca181cde6babc43ff694443451ea68
Garner <garner.pd@pg.com>
2019-01-30 15:57:08 -0500

    remove virtual from document.js

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,6 @@ export default class MyDocument extends Document {
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-TCX44CK');
-              //dataLayer.push({ event: "virtualPageView" });
           `
             }}
           />
