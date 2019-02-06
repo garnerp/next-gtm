204801e1e594f59d61160ce47ebfdc2aac32c32d
Garner <garner.pd@pg.com>
2019-02-06 15:20:35 -0500

    reordered the scripts

--- pages/_document.js
+++ pages/_document.js
@@ -11,21 +11,6 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <style>{`body { margin: 0 } /* custom! */`}</style>
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TCX44CK');
-              window.dataLayer = window.dataLayer || [];
-              dataLayer.push({ event: "virtualPageView" });
-              dataLayer.push({ event: "optimize.activate" });
-          `
-            }}
-          />
           <style>{`.async-hide { opacity: 0 !important}`}</style>
           <script
             dangerouslySetInnerHTML={{
@@ -45,6 +30,20 @@ export default class MyDocument extends Document {
 })
   (window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-TCX44CK':true});
 `
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TCX44CK');
+              window.dataLayer = window.dataLayer || [];
+              dataLayer.push({ event: "virtualPageView" });
+              dataLayer.push({ event: "optimize.activate" });
+          `
             }}
           />
         </Head>
