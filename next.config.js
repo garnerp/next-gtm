7266009033b602979dafa2ae5a398c595a69ce82
Garner <garner.pd@pg.com>
2019-02-06 17:13:25 -0500

    moving scripts around and setting up next.config.js

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicRuntimeConfig: {
+    GTM_CONTAINER_ID: "GTM-TCX44CK"
+  }
+};
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,8 @@
 import Document, { Head, Main, NextScript } from "next/document";
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+const { GTM_CONTAINER_ID } = publicRuntimeConfig;
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -15,21 +19,13 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-(function(a,s,y,n,c,h,i,d,e){
-  s.className+=' '+y;
-  h.start=1*new Date;
-  h.end=i=function(){
-    s.className=s.className.replace(RegExp(' ?'+y),'')
-  };
-  (a[n]=a[n]||[]).hide=h;
-  setTimeout(function(){
-    i();
-    h.end=null
-  },c);
-  h.timeout=c;
-})
-  (window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-TCX44CK':true});
-`
+                window.dataLayer = window.dataLayer || [];
+                (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+                h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+                (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+                })(window,document.documentElement,'async-hide','dataLayer',4000,
+                {'${GTM_CONTAINER_ID}':true});
+              `
             }}
           />
           <script
@@ -39,9 +35,7 @@ export default class MyDocument extends Document {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TCX44CK');
-              window.dataLayer = window.dataLayer || [];
-              dataLayer.push({ event: "virtualPageView" });
+              })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
               dataLayer.push({ event: "optimize.activate" });
           `
             }}
