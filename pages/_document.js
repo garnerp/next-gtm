531b55ad9637080ae84d1892668ede6b38abecf0
Garner <garner.pd@pg.com>
2019-02-06 14:05:07 -0500

    adding in the anti-flicker snippet

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,30 @@ export default class MyDocument extends Document {
           `
             }}
           />
+          <style>{`.async-hide { opacity: 0 !important}`}</style>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+<script>
+(function(a,s,y,n,c,h,i,d,e){
+  s.className+=' '+y;
+  h.start=1*new Date;
+  h.end=i=function(){
+    s.className=s.className.replace(RegExp(' ?'+y),'')
+  };
+  (a[n]=a[n]||[]).hide=h;
+  setTimeout(function(){
+    i();
+    h.end=null
+  },c);
+  h.timeout=c;
+})
+  (window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-TCX44CK':true});
+</script>
+
+`
+            }}
+          />
         </Head>
         <body className="custom_class">
           <Main />
