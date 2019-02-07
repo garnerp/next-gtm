3ea16611486c534f2fef42020d5310e0db79aa03
Peter Garner <garnerp@gmail.com>
2019-02-06 20:45:10 -0500

    scaling back

--- pages/_document.js
+++ pages/_document.js
@@ -19,24 +19,11 @@ export default class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
-                h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
-                (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
-                })(window,document.documentElement,'async-hide','dataLayer',4000,
-                {'${GTM_CONTAINER_ID}':true});
-              `
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
-              dataLayer.push({ event: "optimize.activate" });
           `
             }}
           />
@@ -49,3 +36,25 @@ export default class MyDocument extends Document {
     );
   }
 }
+/*
+
+
+           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+                h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+                (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+                })(window,document.documentElement,'async-hide','dataLayer',4000,
+                {'${GTM_CONTAINER_ID}':true});
+              `
+            }}
+          />
+ 
+
+                       dataLayer.push({ event: "optimize.activate" });
+ 
+                       
+
+  */
