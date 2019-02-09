05d43180767ec5eb17b9541ba82657413232ecca
Peter Garner <garnerp@gmail.com>
2019-02-08 22:12:25 -0500

    small updates

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   publicRuntimeConfig: {
     GTM_CONTAINER_ID: "GTM-TCX44CK"
--- pages/_document.js
+++ pages/_document.js
@@ -26,7 +26,6 @@ export default class MyDocument extends Document {
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
-
           `
             }}
           />
